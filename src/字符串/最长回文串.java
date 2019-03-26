2603508dd88064caff865905d37b829bac57558c
TangDuoBuTiao <296206234@qq.com>
2019-03-26 22:05:22 +0800

    1

--- src/字符串/最长回文串.java
+++ src/字符串/最长回文串.java
@@ -1,19 +1,23 @@
 package 字符串;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Vector;
 
 public class 最长回文串 {
-    public String longestPalindrome(String s) {
-        if (s.isEmpty()) return "";
-        int len = s.length();
-        if (len == 1) return s;
-        int longest = 1, start = 0;
-        Vector<Vector<Integer>> dp = new Vector<>(len);
-        for (int i = 0; i < len; i++) {
-
+    public static void main(String[] args) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        String s = "abbac";
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
+            } else {
+                map.put(s.charAt(i), 1);
+            }
         }
-        String ans = "";
-
-        return ans;
+        for(Character key:map.keySet())
+        {
+            System.out.println("Key: "+key+" Value: "+map.get(key));
+        }
     }
 }
