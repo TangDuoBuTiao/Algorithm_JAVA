7545e967c988baf9eb6f2844186c9dd2c12b4e29
TangDuoBuTiao <296206234@qq.com>
2019-03-29 21:05:21 +0800

    1

--- src/链表/InitializeLinkedList.java
+++ src/链表/InitializeLinkedList.java
@@ -1,5 +1,7 @@
 package 链表;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Stack;
 
 public class InitializeLinkedList {
@@ -26,10 +28,10 @@ public class InitializeLinkedList {
 
     public static void main(String[] args) {
         Node node1 = new Node(1);
-        Node node2 = new Node(2);
+        Node node2 = new Node(1);
         Node node3 = new Node(3);
-        Node node4 = new Node(5);
-        Node node5 = new Node(7);
+        Node node4 = new Node(3);
+        Node node5 = new Node(3);
         node1.next = node2;
         node2.next = node3;
         node3.next = node4;
@@ -47,46 +49,26 @@ public class InitializeLinkedList {
 
         printLinkedList(node1);
         //printLinkedList(nodeA);
-        printLinkedList(reverseKNodes1(node1, 2));
+        printLinkedList(removeRepeatNodes(node1));
     }
 
     // ***********************************************************测试代码放这里
-    public static Node reverseKNodes1(Node head, int k) {
-        if (k < 2) {
-            return head;
-        }
-        Stack<Node> stack = new Stack<>();
-        Node newHead = head;
-        Node pre = null;
-        Node next = null;
+    public static Node removeRepeatNodes(Node head) {
+        HashSet<Integer> set = new HashSet<>();
         Node cur = head;
+        Node pre = null;
         while (cur != null) {
-            next = cur.next;
-            stack.push(cur);
-            if (stack.size() == k) {
-                pre = resign1(stack, pre, next);
-                newHead = newHead == head ? cur : newHead;
+            if (!set.contains(cur.value)) {
+                set.add(cur.value);
+                pre = cur;
+                cur = cur.next;
+                pre.next = cur;
+            } else {
+                pre.next = cur.next;
+                cur = cur.next;
             }
-            cur = next;
         }
-        return newHead;
-
+        return head;
     }
-
-    public static Node resign1(Stack<Node> stack, Node left, Node right) {
-        Node cur = stack.pop();
-        if (left != null) {
-            left.next = cur;
-        }
-        Node next = null;
-        while (!stack.isEmpty()) {
-            next = stack.pop();
-            cur.next = next;
-            cur = next;
-        }
-        cur.next = right;
-        return cur;
-    }
-
 //******************************************************************测试代码
 }
