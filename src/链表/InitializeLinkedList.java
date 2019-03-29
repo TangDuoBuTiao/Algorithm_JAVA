495c769c965ab78068db0182007cc314b6a8fe1d
TangDuoBuTiao <296206234@qq.com>
2019-03-29 20:29:13 +0800

    1

--- src/链表/InitializeLinkedList.java
+++ src/链表/InitializeLinkedList.java
@@ -46,50 +46,47 @@ public class InitializeLinkedList {
         nodeD.next = nodeE;
 
         printLinkedList(node1);
-        printLinkedList(nodeA);
-        printLinkedList(addTwoLinkedList2(node1, nodeA));
+        //printLinkedList(nodeA);
+        printLinkedList(reverseKNodes1(node1, 2));
     }
 
     // ***********************************************************测试代码放这里
-    public static Node addTwoLinkedList2(Node head1, Node head2) {
-        //反转链表过程，pre1是head1反转后的结果，pre2是head2反转后的结果
-        Node next = null;
-        Node pre1 = null;
-        while (head1 != null) {
-            next = head1.next;
-            head1.next = pre1;
-            pre1 = head1;
-            head1 = next;
+    public static Node reverseKNodes1(Node head, int k) {
+        if (k < 2) {
+            return head;
         }
-        Node pre2 = null;
-        while (head2 != null) {
-            next = head2.next;
-            head2.next = pre2;
-            pre2 = head2;
-            head2 = next;
-        }
-        //两个反转后的链表相加过程
-        int n1 = 0, n2 = 0, n = 0, ca = 0;
-        Node node = null;
+        Stack<Node> stack = new Stack<>();
+        Node newHead = head;
         Node pre = null;
-        while (pre1 != null && pre2 != null) {
-            n1 = pre1 != null ? pre1.value : 0;
-            n2 = pre2 != null ? pre2.value : 0;
-            n = n1 + n2 + ca;
-            pre = node;
-            node = new Node(n % 10);
-            node.next = pre;
-            ca = n / 10;
-            pre1 = pre1 != null ? pre1.next : null;
-            pre2 = pre2 != null ? pre2.next : null;
+        Node next = null;
+        Node cur = head;
+        while (cur != null) {
+            next = cur.next;
+            stack.push(cur);
+            if (stack.size() == k) {
+                pre = resign1(stack, pre, next);
+                newHead = newHead == head ? cur : newHead;
+            }
+            cur = next;
         }
-        if (ca == 1) {
-            pre = node;
-            node = new Node(1);
-            node.next = pre;
-        }
-        return node;
+        return newHead;
 
     }
+
+    public static Node resign1(Stack<Node> stack, Node left, Node right) {
+        Node cur = stack.pop();
+        if (left != null) {
+            left.next = cur;
+        }
+        Node next = null;
+        while (!stack.isEmpty()) {
+            next = stack.pop();
+            cur.next = next;
+            cur = next;
+        }
+        cur.next = right;
+        return cur;
+    }
+
 //******************************************************************测试代码
 }
---
+++ src/链表/ReverseKNodes.java
@@ -0,0 +1,56 @@
+package 链表;
+
+import java.util.Stack;
+
+/*
+ 给定一个单链表，使得每K个节点之间逆序，最后不足K个不逆序
+ */
+public class ReverseKNodes {
+    public class Node {
+        public int value;
+        public Node next;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+
+    // 方法一：使用栈结构
+    public Node reverseKNodes1(Node head, int k) {
+        if (head == null || head.next == null || k < 2) {
+            return head;
+        }
+        Stack<Node> stack = new Stack<>();
+        Node newHead = head;
+        Node pre = null;
+        Node next = null;
+        Node cur = head;
+        while (cur != null) {
+            next = cur.next;
+            stack.push(cur);
+            if (stack.size() == k) {
+                pre = resign1(stack, pre, next);
+                newHead = newHead == head ? cur : newHead;   //用第一组反转后的头结点为整个链表的头结点，后面就不改了
+            }
+            cur = next;
+        }
+        return newHead;
+
+    }
+
+    public Node resign1(Stack<Node> stack, Node left, Node right) {
+        Node cur = stack.pop();
+        if (left != null) {   //上一组最后一个节点指向这组的第一个节点
+            left.next = cur;
+        }
+        Node next = null;
+        while (!stack.isEmpty()) {
+            next = stack.pop();
+            cur.next = next;
+            cur = next;
+        }
+        cur.next = right;
+        return cur;
+    }
+
+}
